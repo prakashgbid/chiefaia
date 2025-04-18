9530ba7a315f11ba1626bba9cde63440cadc68dc
prakashmailid <prakashmailid@gmail.com>
2025-04-18 06:15:18 +0000

    feat: Add config file to display all files in the docs folder

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,38 @@
+export default {
+  title: 'ChiefAIa Docs',
+  description: 'Documentation for ChiefAIa project.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Getting Started',
+        items: [
+          { text: 'Introduction', link: '/' }
+        ]
+      },
+      {
+        text: 'Product Requirements',
+        items: [
+          { text: 'Product Requirements', link: '/product-requirements' }
+        ]
+      },
+      {
+        text: 'App Architecture',
+        items: [
+          { text: 'App Architecture', link: '/app-architecture' }
+        ]
+      },
+      {
+        text: 'Code Architecture',
+        items: [
+          { text: 'Code Architecture', link: '/code-architecture' }
+        ]
+      },
+      {
+        text: 'Agreements',
+        items: [
+          { text: 'Agreements', link: '/agreements' }
+        ]
+      },
+    ]
+  }
+}
